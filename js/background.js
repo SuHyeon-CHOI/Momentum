cd0afb47f0db147d5c95d3c6596e381597e4d30b
SuHyeon-CHOI <tngus4869@gmail.com>
2022-08-02 21:52:52 +0900

    create html, css, js files

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = [
+  "0.jpeg", "1.jpg", "2.jpeg", "3.jpeg", "4.png", "5.jpeg", "6.jpg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
